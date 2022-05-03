84405dc59ffa510d9e86572414e87129c69a2d91
Mayank Birkhani <harshbirkhani@gmail.com>
2022-05-03 19:35:09 +0530

    First commit

---
+++ first.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class first{
+    public static void main(String args[]) {
+        System.out.println("Hiii");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name");
+        String name = sc.nextLine();
+
+        System.out.println("Your name is "+ name);
+    }
+}
